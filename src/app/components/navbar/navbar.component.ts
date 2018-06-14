ab85c021c7573def7962605191163f19d1b90bf0
zouhaier slimane <zouhaier.slimane@biat.com.tn>
2018-06-14 11:44:30 +0100

    add roles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authservice: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  isLoggedIn(): boolean {
+    return this._authservice.isLoggedIn();
+  }
+
+  getJwtSubject(): string {
+    return this._authservice.getJwtSubject();
   }
 
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,21 @@ export class AuthenticationService {
             .pipe(map((res: any) => {
                 // login successful if there's a jwt token in the response
                 if (res && res.token) {
+
+                  // Debugging angular-jwt library
+                  // const helper = new JwtHelperService();
+                  // const decodedToken = helper.decodeToken(res.token);
+                  // const expirationDate = helper.getTokenExpirationDate(res.token);
+                  // const isExpired = helper.isTokenExpired(res.token);
+                  // console.log( '****** Jeton decode : ' + JSON.stringify(decodedToken));
+
                     // store username and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify({ username, token: res.token }));
+
+                  //   const cnt = localStorage.getItem('currentUser');
+                  //   const cnt_token_parsed = helper.decodeToken(JSON.parse(cnt).token);
+                  //   //console.log( '****** Jeton decode à partir du lcoal storage: ' + JSON.stringify(cnt_token_parsed) );
+
                 }
             }));
     }
@@ -25,4 +39,16 @@ export class AuthenticationService {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
     }
+
+    isLoggedIn(): boolean {
+        return (localStorage.getItem('currentUser')) ? true : false;
+    }
+    getJwtSubject(): string {
+      const tokenCnt = localStorage.getItem('currentUser');
+      if (tokenCnt) {
+        return new JwtHelperService().decodeToken(JSON.parse(tokenCnt).token).sub;
+      }
+      return null;
+    }
+
 }
